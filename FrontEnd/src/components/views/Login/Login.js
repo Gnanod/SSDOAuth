386a8325216fa8d60ce12d520c99d4e2ded193a7
Vidhanaarachchi S.P <it18078510@my.sliit.lk>
2021-09-28 12:06:57 +0530

    FrontEnd Login Page initialized

--- FrontEnd/src/components/views/Login/Login.js
+++ FrontEnd/src/components/views/Login/Login.js
@@ -7,13 +7,14 @@ import {
     MDBView,
     MDBContainer,
     MDBAnimation,
-    MDBAlert,
+    MDBIcon,
     MDBModal,
+    MDBAlert,
     MDBModalHeader, MDBModalBody, MDBModalFooter, MDBCardImage
 } from 'mdbreact';
 import './Login.css';
 import {MDBCard, MDBCardBody, MDBInput} from 'mdbreact';
-
+import LoginImage from "../../../assets/loginIcon.png"
 export default class Login extends Component {
     constructor(props) {
         super(props);
@@ -98,24 +99,48 @@ export default class Login extends Component {
         return (
             <div id='apppage'>
                 <MDBView>
-                    <MDBMask className='white-text gradient'/>
+                    <MDBMask className='rgba-purple-slight '/>
+                    <MDBRow>
+                        <MDBCol md='12' className='mb-4 text-center'>
+                            <h2 className='display-5 purple-text mb-0 pt-md-5 pt-5'>
+                                <strong>Create your Blogs and share them on</strong>
+                                <a href='#!' className='indigo-text font-weight-bold'>
+                                    <strong> Facebook</strong>
+                                </a>
+                            </h2>
+                            <hr className='hr-light'/>
+                        </MDBCol>
+                    </MDBRow>
                     <MDBContainer
-                        style={{height: '100%', width: '100%', paddingTop: '10rem'}}
-                        className='d-flex justify-content-center white-text align-items-center'
+                        style={{height: '100%', width: '100%', paddingTop: '0rem'}}
+                        className='d-flex justify-content-center purple-text align-items-center'
                     >
-                        <MDBRow>
-                            <MDBCol md='12' className='text-center text-md-left mt-xl-5 mb-5'>
-                                <MDBAnimation type='fadeInLeft' delay='.3s'>
-                                    <MDBCard className="loginContainer">
+                        <MDBRow className=' align-items-center'>
+                            <MDBCol md='6' className=' text-center text-md-left mb-5'>
+                                {/*<MDBAnimation type='fadeInLeft' delay='.3s'>*/}
+                                    <img src={LoginImage}
+                                         className="figure-img imageheight img-fluid "
+                                         alt="" />
+                                {/*</MDBAnimation>*/}
+                            </MDBCol>
+
+
+                            <MDBCol md='5' className='text-center text-md-left mb-5'>
+                                <MDBAnimation type='fadeInRight' delay='.3s'>
+                                    <MDBCard className='dark-grey-text '>
                                         <form className="needs-validation"
                                               onSubmit={this.validateUser}>
                                             <MDBCardBody className="mx-4">
-                                                <div className="text-center">
-                                                    <h3 className="dark-grey-text mb-5">
-                                                        <strong> Login</strong>
+                                                <div className='text-center'>
+                                                    <h3 className='grey-text mb-5 mt-4 font-weight-bold'>
+                                                        <strong>SIGN</strong>
+                                                        <a href='#!' className='purple-text font-weight-bold'>
+                                                            <strong> IN</strong>
+                                                        </a>
                                                     </h3>
                                                 </div>
                                                 <MDBInput label="Your email" group type="email" validate error="wrong"
+                                                          labelClass='purple-text'
                                                           success="right" value={this.state.loginEmail}
                                                           onChange={this.onChangeEmailV}/>
                                                 {
@@ -127,6 +152,7 @@ export default class Login extends Component {
 
                                                 {/*<input value={this.state.loginPass} onChange={this.onChangePassV}/>*/}
                                                 <MDBInput label="Your password" group type="password" validate
+                                                          labelClass='purple-text'
                                                           containerClass="mb-0" value={this.state.loginPass}
                                                           onChange={this.onChangePassV}/>
                                                 {
@@ -137,25 +163,35 @@ export default class Login extends Component {
                                                 }
 
                                                 <div className="text-center mb-3">
-                                                    <MDBBtn type="submit" color='blue'
-                                                            className="btn-block z-depth-1a buttonSign ">
-                                                        LOGIN
+                                                    <MDBBtn gradient="purple" type="submit"
+                                                            className="btn-block z-depth-1a  ">
+                                                        SIGN IN
                                                     </MDBBtn>
+                                                    <MDBCol className='d-flex  justify-content-center '>
+
+                                                        <MDBBtn
+                                                            color="primary" size="sm" floating social="fb">
+                                                            <MDBIcon fab icon="facebook-f"/>
+                                                        </MDBBtn>
+                                                        <MDBBtn color="danger" size="sm" floating social="gplus">
+                                                            <MDBIcon fab icon="google"/>
+                                                        </MDBBtn>
+                                                    </MDBCol>
                                                 </div>
-                                                <MDBModalFooter className="mx-5 pt-3 mb-1">
-                                                    <p className="font-small grey-text d-flex justify-content-end">
-                                                        Not a member?
-                                                        <MDBBtn outline color="info" size="sm" onClick={this.toggle2}>
-                                                            Sign Up</MDBBtn>
+
+                                                <MDBCol className="mx-5 pt-3 mb-1 d-flex" md='12'>
+                                                    <p className='font-small grey-text d-flex justify-content-end'>
+                                                        Do not have an account?
+                                                        <a href='#!' className='purple-text ml-1 font-weight-bold'>
+                                                            Sign Up
+                                                        </a>
                                                     </p>
-                                                </MDBModalFooter>
+                                                </MDBCol>
                                             </MDBCardBody>
                                         </form>
-
                                     </MDBCard>
                                 </MDBAnimation>
                             </MDBCol>
-
                         </MDBRow>
                     </MDBContainer>
                 </MDBView>
