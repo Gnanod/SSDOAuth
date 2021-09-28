4c30e4ba5c1b489dadf3937e20d62d85420f524e
Gunasekara R.P.T.I <it18051780@my.sliit.lk>
2021-09-28 09:00:16 +0530

    Implemented credentials json and get auth url

--- SSDNodeAuthBackend/routes/oauthRoutes.js
+++ SSDNodeAuthBackend/routes/oauthRoutes.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const {google} = require('googleapis');
+const credentials = require('../credentials.json');
 
-router.post('/checkRoute', (req, res) => {
-    res.send("Router Correct");
+const client_id = credentials.web.client_id;
+const client_secret = credentials.web.client_secret;
+const redirect_uris = credentials.web.redirect_uris;
+const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+const SCOPE = ['https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive.file']
+
+router.get('/getAuthURL', (req, res) => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPE,
+    });
+    return res.send(authUrl);
 });
 
 module.exports = router;
