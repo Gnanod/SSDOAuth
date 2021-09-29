cf8333bea587881c3c41a61f23b6e868d1f77fe7
DivyaniRajapaksha <divyanirajapaksha@gmail.com>
2021-09-29 10:16:37 +0530

    File upload Loader implementation

---
+++ FrontEnd/src/components/views/admin/Files/loader.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import CircularProgress from "@material-ui/core/CircularProgress/CircularProgress";
+import Alert from '@material-ui/lab/Alert';
+
+export const ProgressLoader = ({message}) => {
+
+    return (
+        <div>
+            <Alert icon={false} variant="filled" severity="info">
+                <div>
+                    <CircularProgress
+                        size={15}
+                        className="text-light text-center"
+                        style={{marginRight: "25px"}}
+                    />
+                    {" "}
+                    <span style={{float: "center"}} className="text-center"> {message} </span>
+                </div>
+
+            </Alert>
+        </div>
+
+    )
+};
