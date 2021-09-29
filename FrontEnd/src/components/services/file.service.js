11589436b8bd73f5faa49ff9a09a6a463afb7d81
Vidhanaarachchi S.P <it18078510@my.sliit.lk>
2021-09-29 12:26:22 +0530

    Read google drive files using auth (file.service.js)

--- FrontEnd/src/components/services/file.service.js
+++ FrontEnd/src/components/services/file.service.js
@@ -1,6 +1,31 @@
 import axios from "axios";
 import constants from "../../util/constants/Constants";
 
+export const readGDriveFiles = (data) => {
+
+    return axios.request({
+        url: constants.backend_url + 'api/oauth/readDrive',
+        method: 'GET',
+        headers: {
+            Accept: "application/json",
+            'Authorization': localStorage.getItem("token")
+        }
+    }).then(res => {
+        return res;
+    }).catch(err => {
+        console.log(err);
+        console.log(err.data);
+        throw err.response;
+    })
+    // return axios.post(constants.backend_url + 'api/oauth/readDrive', data)
+    //     .then(res => {
+    //         console.log(res);
+    //         return res;
+    //     }).catch(function (error) {
+    //         return error;
+    //     })
+};
+
 export const uploadFile = (data) => {
     return axios.request({
         url: constants.backend_url + 'api/oauth/fileUpload',
